619cbeab5fe892de93ea0c7118047b8b11856f78
DarioneNazionale <56448497+DarioneNazionale@users.noreply.github.com>
2019-11-16 23:02:09 +0100

    last version of the utils file
    commentend and corrected

--- utils.py
+++ utils.py
@@ -1,46 +1,45 @@
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
-from math import log
+from math import log, sqrt
 
-
-def preprocess(s):
+def preprocess(string):
     # Remove punctuation and lower all characters
-    words = nltk.word_tokenize(s)
-    words = [word.lower() for word in words if word.isalnum()]
+    wordsList = nltk.word_tokenize(string)
+    wordsList = [word.lower() for word in wordsList if word.isalnum()]
 
     # Remove stop words
     stop_words = set(stopwords.words('english'))
-    words = [i for i in words if i not in stop_words]
+    wordsList = [i for i in wordsList if i not in stop_words]
 
     # Stemming
     stemmer = PorterStemmer()
-    words = [stemmer.stem(word) for word in words]
+    wordsList = [stemmer.stem(word) for word in wordsList]
 
-    return words
+    return wordsList
 
 def computeTfIdf(wordId, listOfStrings, vocabulary, indexDictionary, allFiles = 30000):
 
     tf = listOfStrings.count(vocabulary[wordId]) / len(listOfStrings)  # compute tf
 
-    idf = 1.0 + log( allFiles / len(indexDictionary[wordId]))
+    idf = 1.0 + log( allFiles / len(indexDictionary[wordId])) # compute idf
 
     return tf * idf  # make the product to find the tfIdf
 
 
-def CountCosineSimilarity(ListsearchTFIDF, ListdocumentTFIDF):
+def CountCosineSimilarity(queryListsTFIDF, articleListTFIDF):
+
+    #doing the dot product between articles and querys:
     dotProduct = 0
-    for i in range(len(ListsearchTFIDF)):
-        dotProduct = dotProduct + (ListsearchTFIDF[i] * ListdocumentTFIDF[i])
+    for i in range(len(queryListsTFIDF)):
+        dotProduct = dotProduct + (queryListsTFIDF[i] * articleListTFIDF[i])
 
-    queryNorm = 0
-    for i in range(len(ListsearchTFIDF)):
-        queryNorm += sqrt((ListsearchTFIDF[i]) * (ListsearchTFIDF[i]))
+    # find the norm of the query
+    queryNorm = sum([sqrt((queryListsTFIDF[i]) * (queryListsTFIDF[i])) for i in range(len(queryListsTFIDF))])
 
-    documentNorm = 0
-    for i in range(len(ListdocumentTFIDF)):
-        documentNorm += sqrt((ListdocumentTFIDF[i]) * (ListdocumentTFIDF[i]))
+    # find the norm of the article
+    documentNorm = sum([sqrt((articleListTFIDF[i]) * (articleListTFIDF[i])) for i in range(len(articleListTFIDF))])
 
-    return (dotProduct / (queryNorm * documentNorm))
+    return (dotProduct / (queryNorm * documentNorm)) #retun the cosin similarity:
